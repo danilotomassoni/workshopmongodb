6ce2354bf04c984d9cd281fd6d9f0e75663c6c3e
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-04-06 17:38:11 -0300

    Created Respository and Service User and connecting MongoDB

--- src/main/java/com/workshopmongodb/domain/User.java
+++ src/main/java/com/workshopmongodb/domain/User.java
@@ -3,8 +3,13 @@ package com.workshopmongodb.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
 public class User implements Serializable{
 
+    @Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/workshopmongodb/resources/UserResource.java
@@ -1,23 +1,27 @@
 package com.workshopmongodb.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.workshopmongodb.domain.User;
+import com.workshopmongodb.services.UserService;
 
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
     @GetMapping
-    public List<User> findAll() {
-        List<User> list = new ArrayList<>();      
-        return list;
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
     
     

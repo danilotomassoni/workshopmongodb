7b30653e052d49ceafcce4903a75b0e0ad18f829
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-04-06 17:23:09 -0300

    Entity user and Rest API

---
+++ src/main/java/com/workshopmongodb/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.workshopmongodb.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workshopmongodb.domain.User;
+
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @GetMapping
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();      
+        return list;
+    }
+    
+    
+}

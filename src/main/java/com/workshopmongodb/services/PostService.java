af32689574420ce4125a4b6b5c64e197f625914d
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-04-12 19:54:29 -0300

    Post findById

---
+++ src/main/java/com/workshopmongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshopmongodb.domain.Post;
+import com.workshopmongodb.repositories.PostRepository;
+import com.workshopmongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id){
+        Optional<Post> post = repository.findById(id);
+        if(post.isEmpty()){
+            throw  new ObjectNotFoundException("Object not found.");
+        }
+        return post.get();
+    }
+
+}
